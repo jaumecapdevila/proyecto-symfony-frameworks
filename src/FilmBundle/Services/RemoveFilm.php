ffb6ab2fd8364780efc894733c88110a2c9a17b7
Jaume Capdevila Pérez <jaumecapdevila93@gmail.com>
2016-03-08 21:36:53 +0100

    Now the service  removeFilm uses the serivce searchFilmById in order to get the film to remove

--- src/FilmBundle/Services/RemoveFilm.php
+++ src/FilmBundle/Services/RemoveFilm.php
@@ -1,21 +1,26 @@
 <?php
 
 namespace FilmBundle\Services;
+
 use Doctrine\ORM\EntityManager;
 use FilmBundle\Entity\Film;
 
 class RemoveFilm
 {
     private $entityManager;
+    /** @var SearchFilmById  */
+    private $searchFilmByIdService;
 
-    public function __construct(EntityManager $entityManager)
+    public function __construct(EntityManager $entityManager, SearchFilmById $searchFilmById)
     {
         $this->entityManager = $entityManager;
+        $this->searchFilmByIdService = $searchFilmById;
     }
 
-    public function __invoke(Film $filmToRemove)
+    public function __invoke($id)
     {
-        $this->entityManager->remove($filmToRemove);
+        $film = $this->searchFilmByIdService($id);
+        $this->entityManager->remove($film);
         $this->entityManager->flush();
     }
 }
