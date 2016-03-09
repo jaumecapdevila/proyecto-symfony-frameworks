5ac2e9fcdc81068d9b41749405ca23837472236e
Pablo Puga <pablo.puga.git@gmail.com>
2016-03-09 20:26:39 +0100

    The EditFilmController reads now the content of the request to get the film parameters

--- src/FilmBundle/Controller/EditFilmController.php
+++ src/FilmBundle/Controller/EditFilmController.php
@@ -14,8 +14,7 @@ class EditFilmController extends Controller
 {
     public function editAction(Request $request, $id)
     {
-        $filmJson = '{"name":"Test","year":2009,"date":"19/07/2009","url":"http://www.imdb.com/title/tt0361748/?ref_=fn_al_tt_1"}';
-        $filmInfo = json_decode($filmJson, true);
+        $filmInfo = json_decode($request->getContent(), true);
 
         $date = DateTime::createFromFormat('d/m/Y', $filmInfo["date"]);
         $editedFilm = new Film($filmInfo["name"], $filmInfo["year"], $date, $filmInfo["url"]);
