e8206dc1d1e26edf99dbcb1b4ab57bf7be2bf414
Jaume Capdevila Pérez <jaumecapdevila93@gmail.com>
2016-03-09 15:19:19 +0100

    Added the ListFilmsCotroller who responds to a GET petition to /film path and gets back a response containing a json with all the films

---
+++ src/FilmBundle/Controller/ListFilmsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace FilmBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use FilmBundle\Entity\Film;
+use FilmBundle\Services\ListFilms;
+
+class ListFilmsController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        $listFilms = $this->get('list.films');
+        $allFilms = $listFilms();
+        $rows     = [];
+        /** @var Film $film */
+        foreach ($allFilms as $film) {
+            $rows[] = [
+                $film->getId(),
+                $film->getName(),
+                $film->getYear(),
+                $film->getDate()->format('d/m/Y'),
+                $film->getImdbUrl()
+            ];
+        }
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setContent(json_encode($rows));
+
+        return $response;
+    }
+}
+
