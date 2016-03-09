d9449b543f5f87fb376a1e4197025c3ec6beb369
Jaume Capdevila Pérez <jaumecapdevila93@gmail.com>
2016-03-09 16:05:14 +0100

    Created the RemoveFilmController that listens to the /remove{id}/film route uses the RemoveFilm service for removing the specified film

---
+++ src/FilmBundle/Controller/RemoveFilmController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FilmBundle\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Config\Definition\Exception\Exception;
+
+class RemoveFilmController extends Controller
+{
+    public function removeAction($id)
+    {
+        $removeFilm = $this->get('remove.film');
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+
+        try {
+            $removeFilm($id);
+            $response->setContent('{"message":"OK"');
+        } catch (Exception $e) {
+            $response->setContent('{"message":"Film doesn\'t exist"}');
+        }
+
+        return $response;
+    }
+}
