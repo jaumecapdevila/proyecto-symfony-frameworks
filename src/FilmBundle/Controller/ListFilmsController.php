0177f47fdcc6eeb581b8655ac61c95b1c8c269a2
Pablo Puga <pablo.puga.git@gmail.com>
2016-03-10 19:56:19 +0100

    Fixed a bug where an Exception was not being catched

--- src/FilmBundle/Controller/ListFilmsController.php
+++ src/FilmBundle/Controller/ListFilmsController.php
@@ -3,6 +3,8 @@
 namespace FilmBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FilmBundle\Entity\Film;
@@ -12,26 +14,30 @@ class ListFilmsController extends Controller
 {
     public function listAction(Request $request)
     {
-        /** @var ListFilms $listFilms */
-        $listFilms = $this->get('list.films');
-        /** @var ListFilms $listFilms */
-        $allFilms = $listFilms();
-        $rows     = [];
-        /** @var Film $film */
-        foreach ($allFilms as $film) {
-            $rows[] = [
-                "id" => $film->getId(),
-                "name" => $film->getName(),
-                "year" => $film->getYear(),
-                "date" => $film->getDate()->format('d/m/Y'),
-                "imdbUrl" => $film->getImdbUrl()
-            ];
-        }
-        $response = new Response();
-        $response->headers->set('Content-Type', 'application/json');
-        $response->setContent(json_encode($rows));
+        try {
+            /** @var ListFilms $listFilms */
+            $listFilms = $this->get('list.films');
+            /** @var ListFilms $listFilms */
+            $allFilms = $listFilms();
+            $rows     = [];
+            /** @var Film $film */
+            foreach ($allFilms as $film) {
+                $rows[] = [
+                    "id" => $film->getId(),
+                    "name" => $film->getName(),
+                    "year" => $film->getYear(),
+                    "date" => $film->getDate()->format('d/m/Y'),
+                    "imdbUrl" => $film->getImdbUrl()
+                ];
+            }
+            $response = new Response();
+            $response->headers->set('Content-Type', 'application/json');
+            $response->setContent(json_encode($rows));
 
-        return $response;
+            return $response;
+        } catch (Exception $e) {
+            return new JsonResponse([]);
+        }
     }
 }
 
